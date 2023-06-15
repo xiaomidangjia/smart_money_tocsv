7f5816bf4d1f0a1b66e62d0fbf431b1cbcd322fb
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-06-15 23:35:00 +0800

    Add files via upload

--- send_picture.py
+++ send_picture.py
@@ -1,4 +1,5 @@
 import telegram
+import datetime
 bot = telegram.Bot(token='6219784883:AAE3YXlXvxNArWJu-0qKpKlhm4KaTSHcqpw')
 bot.sendDocument(chat_id='-840309715', document=open('/root/usdt_predict/out/未来24小时BTC价格趋势预测.png', 'rb'))
 import pandas as pd
